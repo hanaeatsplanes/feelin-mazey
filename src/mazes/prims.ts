32f4fcc95c2ce4d00f3c0c0380fcf97aca4f2b09
sahana <hana@girlin.tech>
2026-04-04 14:56:49 -0400

    prims

--- src/mazes/prims.ts
+++ src/mazes/prims.ts
@@ -1,7 +1,37 @@
-import {findNeighborsTrim} from "@/mazes/utils/gridworld.ts";
+import { findNeighborsTrim } from "@/mazes/utils.ts";
 
 export default function (width: number, height: number) {
-    const size = width * height;
-    const nbrs = findNeighborsTrim(width, height);
-    const maze = Array(size).fill(0)
+	const size = width * height;
+	const nbrs = findNeighborsTrim(width, height);
+	const visited = new Set<number>();
+	const edges: [number, number][] = [];
+
+	const start = Math.floor(Math.random() * size);
+	visited.add(start);
+
+	let frontier: [number, number][] = [];
+	for (const n of nbrs.get(start)!) {
+		frontier.push([start, n]);
+	}
+
+	while (frontier.length > 0) {
+		const idx = Math.floor(Math.random() * frontier.length);
+		const wall = frontier[idx];
+		const [, cell] = wall;
+
+		if (!visited.has(cell)) {
+			visited.add(cell);
+			edges.push(wall);
+
+			for (const n of nbrs.get(cell)!) {
+				if (!visited.has(n)) {
+					frontier.push([cell, n]);
+				}
+			}
+		}
+
+		frontier.splice(idx, 1);
+	}
+
+	return edges;
 }
--- src/pages/api/mazes/generate/[width]/[length]/[type]/index.ts
+++ src/pages/api/mazes/generate/[width]/[length]/[type]/index.ts
@@ -1,14 +1,28 @@
-export async function GET({ params }: { params: { width: string, length: string, type: string }}) {
-    const { width: sWidth, length: sLength, type } = params;
-    const width = Number(sWidth)
-    const length = Number(sLength)
+import prims from "@/mazes/prims.ts";
 
-    if (Number.isNaN(width) || Number.isNaN(length) || width < 1 || length < 1) {
-        return new Response(JSON.stringify({error: "Width and length must be a number greater than 1"}))
-    }
+export async function GET({
+	params,
+}: {
+	params: { width: string; length: string; type: string };
+}) {
+	const { width: sWidth, length: sLength, type } = params;
+	const width = Number(sWidth);
+	const length = Number(sLength);
 
-    switch (type) {
-        case "prim":
-            break;
-    }
+	if (Number.isNaN(width) || Number.isNaN(length) || width < 1 || length < 1) {
+		return new Response(
+			JSON.stringify({ error: "Width and length must be a number greater than 1" }), {
+                status: 400,
+                headers: { "Content-Type": "application/json" },
+            }
+		);
+	}
+
+    let maze = prims(width, length);
+
+	return new Response(
+        JSON.stringify({ maze }), {
+            headers: { "Content-Type": "application/json" },
+        }
+    )
 }
